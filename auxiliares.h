863dae6ff3e138bdfd5f7c112e8d5e6c1a8b3861
Gustavo Irineu <101259023+gustavoib@users.noreply.github.com>
2023-11-24 19:38:09 -0300

    adição de novas funções

--- auxiliares.h
+++ auxiliares.h
@@ -5,9 +5,26 @@
 
 using namespace std;
 
-vector<double> substituicoesRetroativas(double matriz[][3], double *b, int n);
+vector<vector<double>> escalonarMatriz(const vector<vector<double>> &matriz);
 
-void geradorDeLU(int n, double matriz[][3], double matrizL[][3]);
+vector<vector<double>> gerarMatrizD(const vector<vector<double>> &matriz);
 
-void printaMatriz(double matriz[][3]);
+void imprimirMatriz(const vector<vector<double>> &matriz);
+
+vector<vector<double>> gerarMatrizP(const vector<vector<double>> &matriz);
+
+vector<double> resolverSistema(vector<vector<double>> matrizA, vector<double> vetorB);
+
+void imprimirVetor(const vector<double> &vetor);
+
+// void fatoracaoLDP(int n, double matrizU[][3], double matrizL[][3], double *f);
+
+//vector<double> resolucaoAxb(int n, vector<vector<double>> matriz, vector<double> &b);
+
+// vector<double> substituicoesRetroativas(double matriz[][3], double *b, int n);
+
+// void geradorDeLU(int n, double matriz[][3], double matrizL[][3]);
+
+// void printaMatriz(double matriz[][3]);
+
 #endif //TRAB2MN1_AUXILIARES_H
